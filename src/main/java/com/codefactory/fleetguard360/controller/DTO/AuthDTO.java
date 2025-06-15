cb82fe4a045eb4aa71319ed578d3c0ef27c5b04d
Arboleda5252 <118305325+Arboleda5252@users.noreply.github.com>
2025-06-15 11:23:57 -0500

    Update AuthDTO.java

--- src/main/java/com/codefactory/fleetguard360/controller/DTO/AuthDTO.java
+++ src/main/java/com/codefactory/fleetguard360/controller/DTO/AuthDTO.java
@@ -1,27 +1,39 @@
 package com.codefactory.fleetguard360.controller.dto;
 
 public class AuthDTO {
+    // Constructor privado para evitar la creación de instancias de AuthDTO
+    private AuthDTO() {
+        // Constructor vacío, no realiza ninguna acción
+    }
+
     public static class LoginRequest {
         private String correo;
         private String password;
+
         public String getCorreo() {
             return correo;
         }
+
         public void setCorreo(String correo) {
             this.correo = correo;
         }
+
         public String getPassword() {
             return password;
         }
+
         public void setPassword(String password) {
             this.password = password;
         }
     }
+
     public static class TokenResponse {
         private String token;
+
         public TokenResponse(String token) {
             this.token = token;
         }
+
         public String getToken() {
             return token;
         }
