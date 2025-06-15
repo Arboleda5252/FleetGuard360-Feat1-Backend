25f6a276632d1ec5adf7bd8a1523b0aeb104db50
Arboleda5252 <118305325+Arboleda5252@users.noreply.github.com>
2025-06-15 12:00:14 -0500

    Update RutaController.java

--- src/main/java/com/codefactory/fleetguard360/controller/RutaController.java
+++ src/main/java/com/codefactory/fleetguard360/controller/RutaController.java
@@ -1,47 +1,35 @@
-package com.codefactory.fleetguard360.controller;
 
+package com.codefactory.fleetguard360.controller;
 import com.codefactory.fleetguard360.controller.DTO.RutaDTO;
 import com.codefactory.fleetguard360.repository.entities.Ruta;
 import com.codefactory.fleetguard360.service.RutaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-
 @RestController
 @RequestMapping("/api/rutas")
 public class RutaController {
-
-    private final RutaService rutaService;
-
-    // Constructor para la inyección de dependencias
     @Autowired
-    public RutaController(RutaService rutaService) {
-        this.rutaService = rutaService;
-    }
 
+    private RutaService rutaService;
     @PostMapping
     public ResponseEntity<Ruta> crear(@RequestBody RutaDTO dto) {
         return ResponseEntity.ok(rutaService.crearRuta(dto));
     }
-
     @PutMapping("/{id}")
     public ResponseEntity<Ruta> actualizar(@PathVariable int id, @RequestBody RutaDTO dto) {
         return ResponseEntity.ok(rutaService.actualizarRuta(id, dto));
     }
-
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminar(@PathVariable int id) {
         rutaService.eliminarRuta(id);
         return ResponseEntity.noContent().build();
     }
-
     @GetMapping
     public ResponseEntity<List<Ruta>> listar() {
         return ResponseEntity.ok(rutaService.listarRutas());
     }
-
     @GetMapping("/{id}")
     public ResponseEntity<Ruta> obtener(@PathVariable int id) {
         return ResponseEntity.ok(rutaService.obtenerRutaPorId(id));
