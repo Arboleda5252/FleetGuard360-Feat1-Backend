8d03eca5782f90ca9888d713297e53eabfc8e016
Arboleda5252 <118305325+Arboleda5252@users.noreply.github.com>
2025-06-15 11:18:17 -0500

    Update AuthDTO.java

--- src/main/java/com/codefactory/fleetguard360/controller/DTO/AuthDTO.java
+++ src/main/java/com/codefactory/fleetguard360/controller/DTO/AuthDTO.java
@@ -1,37 +1,29 @@
-package com.codefactory.fleetguard360.controller.DTO;  // Cambié dto por DTO
+package com.codefactory.fleetguard360.controller.dto;
 
 public class AuthDTO {
     public static class LoginRequest {
         private String correo;
         private String password;
-        
         public String getCorreo() {
             return correo;
         }
-        
         public void setCorreo(String correo) {
             this.correo = correo;
         }
-        
         public String getPassword() {
             return password;
         }
-        
         public void setPassword(String password) {
             this.password = password;
         }
     }
-    
     public static class TokenResponse {
         private String token;
-        
         public TokenResponse(String token) {
             this.token = token;
         }
-        
         public String getToken() {
             return token;
         }
     }
 }
-
