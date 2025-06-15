fcaee42a0c30d4267fdd5e802bd8a5b753844d0a
Arboleda5252 <118305325+Arboleda5252@users.noreply.github.com>
2025-06-15 11:30:29 -0500

    Update ConductorDTO.java

--- src/main/java/com/codefactory/fleetguard360/controller/DTO/ConductorDTO.java
+++ src/main/java/com/codefactory/fleetguard360/controller/DTO/ConductorDTO.java
@@ -1,5 +1,4 @@
-
-package com.codefactory.fleetguard360.controller.dto;  // Cambié DTO por dto
+package com.codefactory.fleetguard360.controller.DTO;
 
 public class ConductorDTO {
     private int id;
@@ -8,52 +7,41 @@ public class ConductorDTO {
     private String telefono;
     private String email;
     private String password;
-
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getNombre() {
         return nombre;
     }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
     public String getDocumentoIdentidad() {
         return documentoIdentidad;
     }
-
     public void setDocumentoIdentidad(String documentoIdentidad) {
         this.documentoIdentidad = documentoIdentidad;
     }
-
     public String getTelefono() {
         return telefono;
     }
-
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
 }
+
