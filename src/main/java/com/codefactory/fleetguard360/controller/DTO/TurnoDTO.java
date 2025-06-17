8a8b7faaf23b0462b0baae9ea52c8ac38be4344c
Arboleda5252 <118305325+Arboleda5252@users.noreply.github.com>
2025-06-17 11:43:13 -0500

    Update TurnoDTO.java

--- src/main/java/com/codefactory/fleetguard360/controller/DTO/TurnoDTO.java
+++ src/main/java/com/codefactory/fleetguard360/controller/DTO/TurnoDTO.java
@@ -1,51 +1,13 @@
 package com.codefactory.fleetguard360.controller.dto;
 
 import java.util.Date;
+import lombok.Data;
 
+@Data
 public class TurnoDTO {
     private Date inicio;
     private Date fin;
     private String estado;
     private int conductorId;
-    private Integer rutaId;  // Changed from public to private
-
-    public Date getInicio() {
-        return inicio;
-    }
-
-    public void setInicio(Date inicio) {
-        this.inicio = inicio;
-    }
-
-    public Date getFin() {
-        return fin;
-    }
-
-    public void setFin(Date fin) {
-        this.fin = fin;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public int getConductorId() {
-        return conductorId;
-    }
-
-    public void setConductorId(int conductorId) {
-        this.conductorId = conductorId;
-    }
-
-    public Integer getRutaId() {
-        return rutaId;
-    }
-
-    public void setRutaId(Integer rutaId) {
-        this.rutaId = rutaId;
-    }
+    private Integer rutaId;
 }
