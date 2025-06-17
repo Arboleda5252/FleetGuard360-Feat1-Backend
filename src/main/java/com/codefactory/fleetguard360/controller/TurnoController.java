bc6b025adb7a1e05f174f5bc2574184138e3d8aa
Arboleda5252 <118305325+Arboleda5252@users.noreply.github.com>
2025-06-17 09:45:01 -0500

    Update TurnoController.java

--- src/main/java/com/codefactory/fleetguard360/controller/TurnoController.java
+++ src/main/java/com/codefactory/fleetguard360/controller/TurnoController.java
@@ -2,21 +2,20 @@ package com.codefactory.fleetguard360.controller;
 
 import com.codefactory.fleetguard360.controller.DTO.TurnoDTO;
 import com.codefactory.fleetguard360.controller.DTO.TurnoResponseDTO;
-import com.codefactory.fleetguard360.repository.entities.Turno;
 import com.codefactory.fleetguard360.service.TurnoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/turnos")
 public class TurnoController {
 
-    @Autowired
-    private TurnoService turnoService;
+    private final TurnoService turnoService;
+
+    // Constructor injection
+    public TurnoController(TurnoService turnoService) {
+        this.turnoService = turnoService;
+    }
 
     @PostMapping
     public ResponseEntity<TurnoResponseDTO> asignarTurno(@RequestBody TurnoDTO dto) {
