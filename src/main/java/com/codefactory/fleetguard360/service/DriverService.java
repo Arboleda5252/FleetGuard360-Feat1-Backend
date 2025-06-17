d5155125f437c3ca64b3e10db8a306f866c2e5d4
Arboleda5252 <118305325+Arboleda5252@users.noreply.github.com>
2025-06-17 10:31:51 -0500

    Update DriverService.java

--- src/main/java/com/codefactory/fleetguard360/service/DriverService.java
+++ src/main/java/com/codefactory/fleetguard360/service/DriverService.java
@@ -1,14 +1,5 @@
 package com.codefactory.fleetguard360.service;
 
-import org.springframework.stereotype.Service;
-
-@Service
-public class DriverService {
-
-    public String obtenerInfoConductor(int idConductor) {
-        // Lógica para obtener información del conductor por ID
-        return "Información del conductor con ID: " + idConductor;
-    }
-
-    // Puedes agregar más métodos según sea necesario
+public interface DriverService {
+    String obtenerInfoConductor(int idConductor);
 }
