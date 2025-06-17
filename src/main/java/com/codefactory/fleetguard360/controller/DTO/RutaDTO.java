bdda46ac80b846552cad54112e4441fc42e7aeb1
Arboleda5252 <118305325+Arboleda5252@users.noreply.github.com>
2025-06-17 11:36:01 -0500

    Update RutaDTO.java

--- src/main/java/com/codefactory/fleetguard360/controller/DTO/RutaDTO.java
+++ src/main/java/com/codefactory/fleetguard360/controller/DTO/RutaDTO.java
@@ -1,41 +1,12 @@
 package com.codefactory.fleetguard360.controller.dto;
 
+import lombok.Data;
+
+@Data
 public class RutaDTO {
     private int id;
     private String nombre;
     private String origen;
-    private String destino;  // Changed from public to private
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getOrigen() {
-        return origen;
-    }
-
-    public void setOrigen(String origen) {
-        this.origen = origen;
-    }
-
-    public String getDestino() {
-        return destino;
-    }
-
-    public void setDestino(String destino) {
-        this.destino = destino;
-    }
+    private String destino;
 }
 
