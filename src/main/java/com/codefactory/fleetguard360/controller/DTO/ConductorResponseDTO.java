d2aefeba432c60f4684621f7651acc3e71a8f071
Arboleda5252 <118305325+Arboleda5252@users.noreply.github.com>
2025-06-17 11:38:24 -0500

    Update ConductorResponseDTO.java

--- src/main/java/com/codefactory/fleetguard360/controller/DTO/ConductorResponseDTO.java
+++ src/main/java/com/codefactory/fleetguard360/controller/DTO/ConductorResponseDTO.java
@@ -1,26 +1,10 @@
 package com.codefactory.fleetguard360.controller.dto;
 
+import lombok.Data;
+
+@Data
 public class ConductorResponseDTO {
     private String nombre;
     private String documentoIdentidad;
     private String telefono;
-
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    public String getDocumentoIdentidad() {
-        return documentoIdentidad;
-    }
-    public void setDocumentoIdentidad(String documentoIdentidad) {
-        this.documentoIdentidad = documentoIdentidad;
-    }
-    public String getTelefono() {
-        return telefono;
-    }
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
 }
